fee5f083997fa89e17567a3e9f31461afc173336
MaheshB77 <mbansode7696@gmail.com>
2021-09-12 13:17:56 +0530

    Initial commit

---
+++ src/main/java/com/mahesh/sbmysqlhibernatedemo/SbMysqlHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.sbmysqlhibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMysqlHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMysqlHibernateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/sbmysqlhibernatedemo/SbMysqlHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.sbmysqlhibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMysqlHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
